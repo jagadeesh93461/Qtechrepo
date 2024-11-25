de4a3d9872d5df1e454bc48386c9d5681a781f72
jagadeesh93461 <konetijagadeesh7@gmail.com>
2024-11-25 11:35:09 +0530

    added

--- applicaton.java
+++ applicaton.java
@@ -14,5 +14,23 @@ public class HelloWorld {
 
         // println() prints the following line to the output screen
         System.out.println("You entered: " + number);
-    }
+  }
+ #feature203 change by dev1
+# Python program to swap two variables
+
+x = 5
+y = 10
+
+# To take inputs from the user
+#x = input('Enter value of x: ')
+#y = input('Enter value of y: ')
+
+# create a temporary variable and swap the values
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+#feature203 end 
 }
